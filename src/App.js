889cc4e8ec76753652f843c6704793968f38b49b
sameerthecoder94 <sameerthecoder@gmail.com>
2022-02-01 14:59:42 -0500

    Materialize Setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    //Init Materialize JS
+    M.AutoInit();
+  }, []);
   return <div className='App'>IT_Logger_App_Setup</div>;
-}
+};
 
 export default App;
